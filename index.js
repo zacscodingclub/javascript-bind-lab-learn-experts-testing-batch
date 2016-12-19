6842a7b41ce029cde477c8555292eea6075134e6
Zac Baston <zbaston@gmail.com>
2016-12-19 21:41:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round(Date.now()/1000) ,
+  startTicking() {
+    setInterval(() => this.time++, 1000);
+  }
+}
+
+const censor = (word, str) => {
+  const match = new RegExp(word, 'g');
+  return str.replace(match, "BLEEP")
+}
+
+const violenceCensor = censor.bind(null, 'violence');
+const drugsCensor = censor.bind(null, 'drugs');
